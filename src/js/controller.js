51e5a77b5005ffe452c22ba5484ca43ba5011c74
MassMatsu <masa10matsukawa@gmail.com>
2021-07-25 13:38:40 +0900

    rendering spinner while loading single recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,6 @@
-import icons from 'url:../img/icons.svg'  // for Parcel 2. 
+import icons from 'url:../img/icons.svg'; // for Parcel 2.
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 const recipeContainer = document.querySelector('.recipe');
 
@@ -14,8 +16,22 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
+const renderSpinner = function (parentEl) {
+  const markup = `
+    <div class="spinner">
+      <svg>
+        <use href="${icons}#icon-loader"></use>
+      </svg>
+    </div>  
+  `;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markup);
+};
+
 const showRecipe = async function () {
   try {
+    renderSpinner(recipeContainer);
+
     const response = await fetch(
       'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcc7e'
     );
@@ -54,14 +70,18 @@ const showRecipe = async function () {
           <svg class="recipe__info-icon">
             <use href="${icons}#icon-clock"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--minutes">${recipe.cookingTime}</span>
+          <span class="recipe__info-data recipe__info-data--minutes">${
+            recipe.cookingTime
+          }</span>
           <span class="recipe__info-text">minutes</span>
         </div>
         <div class="recipe__info">
           <svg class="recipe__info-icon">
             <use href="${icons}#icon-users"></use>
           </svg>
-          <span class="recipe__info-data recipe__info-data--people">${recipe.servings}</span>
+          <span class="recipe__info-data recipe__info-data--people">${
+            recipe.servings
+          }</span>
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
@@ -93,9 +113,10 @@ const showRecipe = async function () {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-        ${recipe.ingredients.map((ing) => {
-          if (ing.quantity !== null)
-          return `
+        ${recipe.ingredients
+          .map((ing) => {
+            if (ing.quantity !== null)
+              return `
           <li class="recipe__ingredient">
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
@@ -106,8 +127,9 @@ const showRecipe = async function () {
               ${ing.description}
             </div>
           </li>
-          `
-        }).join('')}
+          `;
+          })
+          .join('')}
           
         </ul>
       </div>
@@ -116,7 +138,9 @@ const showRecipe = async function () {
         <h2 class="heading--2">How to cook it</h2>
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${recipe.publisher}</span>. Please check out
+          <span class="recipe__publisher">${
+            recipe.publisher
+          }</span>. Please check out
           directions at their website.
         </p>
         <a
@@ -140,14 +164,3 @@ const showRecipe = async function () {
 };
 
 showRecipe();
-
-
-const arr = ['hello', 'he', 'yo']
-
-const scripts = arr.map((script) => {
-  if (script === 'he') 
-  
-  return script
-})
-
-console.log(scripts.join(''))
